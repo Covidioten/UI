aa263c11dea9835c162e334195cafb4547bd1298
ProdIsMyDev <chris@casualcompany.de>
2021-02-26 10:51:22 +0100

    adding apexcharts

--- src/main.js
+++ src/main.js
@@ -1,47 +1,46 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-import AreaExample from './charts/AreaExample'
-import BarExample from './charts/BarExample'
-import ColumnExample from './charts/ColumnExample'
-import ScatterExample from './charts/ScatterExample'
-import MixedExample from './charts/MixedExample'
-import DonutExample from './charts/DonutExample'
-import RadialBarExample from './charts/RadialBarExample'
-import BubbleExample from './charts/BubbleExample'
-import HeatmapExample from './charts/HeatmapExample'
-import LineExample from './charts/LineExample'
-import VueApexCharts from '../../dist/vue-apexcharts';
+import AreaExample from "./charts/AreaExample";
+import BarExample from "./charts/BarExample";
+import ColumnExample from "./charts/ColumnExample";
+import ScatterExample from "./charts/ScatterExample";
+import MixedExample from "./charts/MixedExample";
+import DonutExample from "./charts/DonutExample";
+import RadialBarExample from "./charts/RadialBarExample";
+import BubbleExample from "./charts/BubbleExample";
+import HeatmapExample from "./charts/HeatmapExample";
+import LineExample from "./charts/LineExample";
+import VueApexCharts from "../public/vue-apexcharts";
 
-Vue.component('apexchart', VueApexCharts)
+Vue.component("apexchart", VueApexCharts);
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // 1. Define route components.
 // These can be imported from other files
 const routes = [
-  { path: '/area', component: AreaExample },
-  { path: '/bar', component: BarExample },
-  { path: '/column', component: ColumnExample },
-  { path: '/mixed', component: MixedExample },
-  { path: '/scatter', component: ScatterExample },
-  { path: '/donut', component: DonutExample },
-  { path: '/radialbar', component: RadialBarExample },
-  { path: '/bubble', component: BubbleExample },
-  { path: '/heatmap', component: HeatmapExample },
-  { path: '/line', component: LineExample }
-]
+  { path: "/area", component: AreaExample },
+  { path: "/bar", component: BarExample },
+  { path: "/column", component: ColumnExample },
+  { path: "/mixed", component: MixedExample },
+  { path: "/scatter", component: ScatterExample },
+  { path: "/donut", component: DonutExample },
+  { path: "/radialbar", component: RadialBarExample },
+  { path: "/bubble", component: BubbleExample },
+  { path: "/heatmap", component: HeatmapExample },
+  { path: "/line", component: LineExample },
+];
 
 const router = new VueRouter({
-  routes: routes
-})
-router.replace('/line')
+  routes: routes,
+});
+router.replace("/line");
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
