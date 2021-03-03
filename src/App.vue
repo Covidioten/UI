1c85436787646122237979cd5cdc2ed85e32fe59
luckyluke96 <lukas.oberfrank6@gmail.com>
2021-03-03 08:42:56 +0100

    Deleted other Charts despite of line chart

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,9 @@
   <div id="app">
 
     <p>
-      <router-link to="/area">Area Chart</router-link>
+      
       <router-link to="/line">Line Chart</router-link>
-      <router-link to="/bar">Bar Chart</router-link>
-      <router-link to="/column">Column Chart</router-link>
-      <router-link to="/mixed">Mixed Chart</router-link>
-      <router-link to="/donut">Donut Chart</router-link>
-      <router-link to="/radialbar">RadialBar Chart</router-link>
-      <router-link to="/bubble">Bubble Chart</router-link>
-      <router-link to="/scatter">Scatter Chart</router-link>
-      <router-link to="/heatmap">Heatmap Chart</router-link>
+      
     </p>
 
     <router-view></router-view>
