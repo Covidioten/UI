7625efdb52bb469a82221aecaa6e1562be633162
luckyluke96 <lukas.oberfrank6@gmail.com>
2021-03-07 19:04:20 +0100

    new component navbar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="navigation-bar">
+     
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavigationBar",
+    data: () => {
+        status: false
+    }
+}
+</script>
