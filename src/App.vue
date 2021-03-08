f41158365803d50b7f56d61d622c40569fc0d3cf
luckyluke96 <lukas.oberfrank6@gmail.com>
2021-03-08 22:07:34 +0100

    add component

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,33 @@
       
     </div>    
     <navigation/>
+    
     <h1>COVID-19 Sentiment Analysis</h1>
     
 
     <p>
       
-      <!--<router-link to="/line">Line Chart</router-link>-->
+    <!--<router-link to="/line">Line Chart</router-link>-->
       
     </p>
+    
+
+    
 
       <router-view></router-view>
 
   </div>
+  
 </template>
 
 <script>
+import InfoSection from './components/InfoSection.vue'
 
 
 export default {
   name: 'app',
   components: {
+    InfoSection
     
   }
 }
---
+++ src/components/InfoSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="info-section">
+     hadfkjahdsflkjahdglkajdhglkaj
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InfoSection",
+    data: () => {
+        
+    }
+}
+</script>
