76b5e833b3ccf7ebf637efd112ac57cbdde3d538
luckyluke96 <lukas.oberfrank6@gmail.com>
2021-03-08 22:53:16 +0100

    added info section

--- src/components/InfoSection.vue
+++ src/components/InfoSection.vue
@@ -1,14 +1,24 @@
 <template>
-    <div id="info-section">
-     hadfkjahdsflkjahdglkajdhglkaj
+  <div class="info">
+   
+    <div>
+       Hi, wir sind die Covidioten und auf dieser Webseite haben wir für dich Twitter-Daten analysiert, 
+       um den Zusammenhang zwischen politischen Maßnahmen und der Stimmung der deutschen Twitter-Welt zu zeigen. 
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "InfoSection",
-    data: () => {
-        
+  name: 'InfoSection',
+  data: function() {
+    return {
+      
     }
+  },
+  methods: {
+    
+        
+  }
 }
 </script>
