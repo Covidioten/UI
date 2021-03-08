79c74a30b116c40e358f5fe6b7193a18b2ae1ba2
luckyluke96 <lukas.oberfrank6@gmail.com>
2021-03-08 22:53:16 +0100

    added info section

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,33 @@
     <div id="top-bar">
       
     </div>    
-    <navigation/>
     
     <h1>COVID-19 Sentiment Analysis</h1>
     
 
     <p>
+
+      <router-link to="/sentimentLine">chart</router-link>
       
-    <router-link to="/info">Info</router-link>
-    <router-link to="/sentimentLine">chart</router-link>
+      <router-link to="/info">Info</router-link>
+      
       
     </p>
-    
 
-    
-
-      <router-view></router-view>
-      
-      
-
-
+    <router-view></router-view>
+   
   </div>
   
 </template>
 
 <script>
-import InfoSection from './components/InfoSection.vue'
+
 
 
 export default {
   name: 'app',
   components: {
-    InfoSection
+    
     
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,20 +2,30 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 
-import Status from "./components/NavigationBar.vue";
 import VueApexCharts from "vue-apexcharts";
 
 import SentimentLineDummy from "./charts/SentimentLineDummy";
+
+import BarExample from "./charts/BarExample";
+import ColumnExample from "./charts/ColumnExample";
+import ScatterExample from "./charts/ScatterExample";
+import MixedExample from "./charts/MixedExample";
+import DonutExample from "./charts/DonutExample";
+import RadialBarExample from "./charts/RadialBarExample";
+import BubbleExample from "./charts/BubbleExample";
+import HeatmapExample from "./charts/HeatmapExample";
+
 import InfoSection from "./components/InfoSection";
 
 Vue.component("apexchart", VueApexCharts);
-Vue.component("navigation", Status);
+
 Vue.use(VueRouter);
 
 // 1. Define route components.
 // These can be imported from other files
 const routes = [
   { path: "/sentimentLine", component: SentimentLineDummy },
+  { path: "/column", component: ColumnExample },
   { path: "/info", component: InfoSection },
 ];
 
